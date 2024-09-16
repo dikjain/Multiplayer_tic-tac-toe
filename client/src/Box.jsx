2e2bdb2582f2a0c4318160f2d0ac3be495293773
dikjain <dikshitmahanot2005@gmail.com>
2024-09-16 18:58:18 +0530

    add

--- client/src/Box.jsx
+++ client/src/Box.jsx
@@ -1,51 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import "./App.css"
-import CrossSvg from './CrossSvg';
-import CircleSvg from './CircleSvg';
+
 
 
 
 function Box({playingas,setplayingas,id,socket,setgamestate,gamestate,checkwin,finishedarraystate,isfinished,setisfinished,currentplayer,setcurrentplayer}) {
-    // const circleSvg = (
-    //     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    //       <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-    //       <g
-    //         id="SVGRepo_tracerCarrier"
-    //         stroke-linecap="round"
-    //         stroke-linejoin="round"
-    //       ></g>
-    //       <g id="SVGRepo_iconCarrier">
-    //         {" "}
-    //         <path
-    //           d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-    //           stroke="#ffffff"
-    //           stroke-width="2"
-    //           stroke-linecap="round"
-    //           stroke-linejoin="round"
-    //         ></path>{" "}
-    //       </g>
-    //     </svg>
-    //   );  
-    //   const crossSvg = (
-    //   <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    //     <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-    //     <g
-    //       id="SVGRepo_tracerCarrier"
-    //       stroke-linecap="round"
-    //       stroke-linejoin="round"
-    //     ></g>
-    //     <g id="SVGRepo_iconCarrier">
-    //       {" "}
-    //       <path
-    //         d="M19 5L5 19M5.00001 5L19 19"
-    //         stroke="#fff"
-    //         stroke-width="1.5"
-    //         stroke-linecap="round"
-    //         stroke-linejoin="round"
-    //       ></path>{" "}
-    //     </g>
-    //   </svg>
-    //   );
+    const circleSvg = (
+<svg
+  width="96"
+  height="96"
+  viewBox="0 0 24 24"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+>
+  <path
+    d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
+    stroke="#ffffff"
+    strokeWidth="2"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  />
+</svg>
+
+      );  
+      const crossSvg = (
+        <svg
+        width="96"
+        height="96"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M19 5L5 19M5 5L19 19"
+          stroke="#ffffff"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+      );
 
 
       const [icon,seticon] = useState(null)
@@ -59,9 +53,9 @@ function Box({playingas,setplayingas,id,socket,setgamestate,gamestate,checkwin,f
           
           if(!icon){
             if(currentplayer == "circle"){   
-              seticon(CircleSvg)
+              seticon(circleSvg)
             }else{
-              seticon(CrossSvg)
+              seticon(crossSvg)
             }
           }
           socket.emit("currentplayer", currentplayer)
@@ -85,10 +79,10 @@ function Box({playingas,setplayingas,id,socket,setgamestate,gamestate,checkwin,f
             setisfinished(winner)            
         }
         if(gamestate.flat()[id] == "circle"){
-          seticon(CircleSvg)
+          seticon(circleSvg)
         }
         if(gamestate.flat()[id] == "cross"){
-            seticon(CrossSvg)
+            seticon(crossSvg)
         }
         
         
